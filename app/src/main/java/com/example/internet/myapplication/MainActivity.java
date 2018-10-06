87dfd90e60f0615273e51859089bedd776d44530
DaviQS007 <davidqs2009@hotmail.com>
2018-10-06 16:56:23 -0300

    Alterações de códigos.

--- app/src/main/java/com/example/internet/myapplication/MainActivity.java
+++ app/src/main/java/com/example/internet/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity  extends AppCompatActivity {
 
@@ -15,6 +17,26 @@ public class MainActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        Button buttonCadastro = (Button) findViewById(R.id.buttonCadastro);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intLogin = new Intent(MainActivity.this, TelaLogin.class);
+                startActivity(intLogin);
+            }
+        });
+
+        buttonCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intCadastro = new Intent(MainActivity.this, TelaCadastro.class);
+                startActivity(intCadastro);
+            }
+        });
     }
 
 }
+
--- app/src/main/java/com/example/internet/myapplication/TelaCadastro.java
+++ app/src/main/java/com/example/internet/myapplication/TelaCadastro.java
@@ -2,6 +2,7 @@ package com.example.internet.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class TelaCadastro extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class TelaCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
